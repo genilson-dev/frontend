36ae267d95a2212f88b13dabfaf2ce9d3d0cd350
genilson-dev <programationwork@gmail.com>
2025-02-21 22:45:48 -0300

    Add inputs de email e password

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,36 @@
+import styles from "./page.module.scss";
+import logo from "@/../public/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
-      
-    </div>
+    <>
+      <div>
+        <Image src={logo} alt="Logo" />
+      </div>
+      <section className={styles.login}>
+        <form action="">
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Digite o E-mail"
+            className={styles.input}
+          />
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Digite sua senha"
+            className={styles.input}
+          />
+          <button type="submit" className={styles.button}>Logar</button>
+        </form>
+        <Link href="/signup" className={styles.text}>
+          Não possui uma conta? <strong>Cadastre-se!</strong>
+        </Link>
+      </section>
+    </>
   );
 }
