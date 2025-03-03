d1083f0e8682d157adf1338dfdef657ec0af91b1
genilson-dev <programationwork@gmail.com>
2025-03-03 20:21:22 -0300

    Add config initials do middleware the segurity

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,6 +2,10 @@ import { NextRequest, NextResponse } from 'next/server';
 "use server";
 export async function middleware(req: NextRequest, res: NextResponse) {
     console.log("Middleware called");
+    const { pathname } = req.nextUrl;
+    if (pathname.startsWith("/_next") || pathname === "/" || pathname === "/login") {
+      return NextResponse.next();
+    } 
     
-  return NextResponse.next();
+  
 }
