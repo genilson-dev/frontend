9792c22e973d3760b7dbec1c8fd40ab5275a2b27
genilson-dev <programationwork@gmail.com>
2025-03-03 21:30:49 -0300

    Add parte inicial das configurações de validação do token

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,11 +1,21 @@
-import { NextRequest, NextResponse } from 'next/server';
-"use server";
-export async function middleware(req: NextRequest, res: NextResponse) {
-    console.log("Middleware called");
-    const { pathname } = req.nextUrl;
-    if (pathname.startsWith("/_next") || pathname === "/" || pathname === "/login") {
-      return NextResponse.next();
-    } 
-    
-  
+import { NextRequest, NextResponse } from "next/server";
+import { getCookiesServer } from "./lib/cookieServer";
+
+export async function middleware(req: NextRequest) {
+  console.log("Middleware called");
+  const { pathname } = req.nextUrl;
+  if (pathname.startsWith("/_next") || pathname === "/") {
+    return NextResponse.next();
+  }
+
+  const token = await getCookiesServer();
+  console.log("Token", token);
+  if (pathname.startsWith("/dashboard")) {
+   if (!token) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));    }
+  }
 }
+
+//  Verificando se o token é válido
+
+
