f0ca4ea3432ed62a4353c973064dc1dcc8c01a7a
genilson-dev <programationwork@gmail.com>
2025-03-06 20:15:08 -0300

    Add reset em todos os arquivo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,23 +1,38 @@
 import logo from "./../../public/logo.svg";
-import { api } from "@/service/api";
 import Link from "next/link";
 import Image from "next/image";
-import styles from './page.module.scss'
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Pagina Home</h1>
-      {/* Este link direciona o usuario quando clicar na logo para a pagina home */}
-      <Link href="/">
-        <Image src={logo} alt="Logo favorito" />
+    <div className={styles.containerCenter}>
+      <>
+      <Link href="#">
+        <Image src={logo} alt="Logo do projeto" />
       </Link>
-      <Link href="/signup" className={styles.text}>
-        <h3>Não possui uma conta? Cadastre-se</h3>
-      </Link>
-      <Link href="/login" className={styles.text}>
-        <h3>Já conta? Faça login!</h3>
-      </Link>
+      <section className={styles.login}>
+        <form className={styles.form}>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Digite seu Email"
+            className={styles.input}
+          />
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Digite sua senha"
+            className={styles.input}
+          />
+          <button type="submit" className={styles.buttonDash}>Acess</button>
+        </form>
+        <Link href="/signup" className={styles.text}>
+          <p>Não tem uma conta?, <strong>Cadastre-se</strong></p>
+        </Link>
+      </section>
+    </>
     </div>
   );
 }
--- frontend/src/seguridade/middleware.ts
+++ frontend/src/seguridade/middleware.ts
@@ -1,9 +1,45 @@
-import { NextRequest, NextResponse } from "next/server";
-import { api } from "@/service/api";
+import { NextRequest, NextResponse } from 'next/server'
+import { getCookieServer } from '@/lib/cookieServer'
+import { api } from '@/service/api'
+
 export async function middleware(req: NextRequest){
+  const { pathname } = req.nextUrl
 
-        console.log("Chamou o middleware");
-        
-        return NextResponse.next();
+  if(pathname.startsWith("/_next") || pathname === "/"){
+    return NextResponse.next();
+  }
+  const token = await getCookieServer();
+  
+  if(pathname.startsWith("/dashboard")){
+    if(!token){
+      return NextResponse.redirect(new URL("/", req.url))
+    }
 
+    const isValid = await validateToken(token)
+    console.log(isValid);
+
+    if(!isValid){
+      return NextResponse.redirect(new URL("/", req.url))
+    }
+  }
+
+  return NextResponse.next();
+
+}
+
+
+async function validateToken(token: string){
+  if (!token) return false;
+
+  try{
+    await api.get("/me", {
+      headers:{
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    return true;
+  }catch(err){
+    return false;
+  }
 }
