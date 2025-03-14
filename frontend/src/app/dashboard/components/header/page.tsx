635b0d2af48ac43ea9ebd4c182d73ff7f1c349e9
genilson-dev <programationwork@gmail.com>
2025-03-13 21:07:42 -0300

    A estilização do header page

--- frontend/src/app/dashboard/components/header/page.tsx
+++ frontend/src/app/dashboard/components/header/page.tsx
@@ -2,17 +2,23 @@ import Link from 'next/link'
 import styles from './styles.module.scss'
 import Image from 'next/image'
 import logo from './../../../../../public/logo.svg'
+import { LogOutIcon } from 'lucide-react'
 
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
-      <div className={styles.headerContainer}>
+      <div className={styles.headerContent}>
         <Link href="/dashboard">
           <Image src={logo} alt='Logo da pizzaria' width={190} height={60} priority={true} quality={100}/>
         </Link>
         <nav>
           <Link href="/dashboard/category" className={styles.link}>Categoria</Link>
           <Link href="/dashboard/product" className={styles.link}>Produto</Link>
+          <form action="">
+            <button type='submit'>
+              <LogOutIcon size={24} color='#fff'/>
+            </button>
+          </form>
 
         </nav>
       </div>
