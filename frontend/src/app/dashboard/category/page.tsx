39dc40be163b63e6dd55a50e497e6b2d10e97b5a
genilson-dev <programationwork@gmail.com>
2025-03-18 19:27:42 -0300

    Add styled page category

--- frontend/src/app/dashboard/category/page.tsx
+++ frontend/src/app/dashboard/category/page.tsx
@@ -1,8 +1,24 @@
-
+import styles from "./page.module.scss"
 export default function Category() {
+  function handleSubmit(event: React.FormEvent) {
+    console.log("Clicou categoria")
+  }
   return (
-    <main>
-        <h1>Category</h1>
+    <main className={styles.container}>
+        <h1>Nova Categoria</h1>
+        <form  className={styles.form}>
+          <input 
+          type="text"
+          name="categoria"
+          placeholder="Nome da categoria"
+          required={true}
+          className={styles.input}
+                    
+          />
+          <button className={styles.button} >
+            cadastrar categoria
+          </button>
+        </form>
     </main>
   )
 }
