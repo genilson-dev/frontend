71296c58cb181a3b2f721a2e3ce3fc3ae8fe05d8
genilson-dev <programationwork@gmail.com>
2025-03-20 22:54:33 -0300

    Add salvo para continuação

--- frontend/src/app/dashboard/product/components/form/index.tsx
+++ frontend/src/app/dashboard/product/components/form/index.tsx
@@ -4,6 +4,9 @@ import styles from './../styles.module.scss'
 import {UploadCloud} from 'lucide-react'
 import Image from 'next/image'
 import { Button } from '@/app/dashboard/components/button'
+import { redirect } from 'next/navigation'
+import { api } from '@/service/api'
+import { getCookieClient } from '@/lib/cookieClient'
 
 interface CategoryProps{
     id: string;
@@ -17,6 +20,38 @@ export default function Form({categories}: FormProps) {
     const [image, setImage] = useState<File>();
     const [previewImage, setPreviewImage] = useState<string>("");
 
+    async function handleRegisterProduct(formData: FormData){
+        const name = formData.get('name-product');
+        const price = formData.get('price');
+        const description = formData.get('description');
+        const categoryIndex = formData.get('category');
+
+        if(!name || !price || !description || !categoryIndex || !image) {
+            console.log('Invalid form data');
+            return;
+        }   
+
+
+        // console.log(categories[Number(categoryIndex)].id);
+        // console.log(categories[Number(categoryIndex)].name);
+        // console.log(categories[Number(categoryIndex)]);
+        const data = new FormData();
+        data.append('name', name);
+        data.append('price', price);
+        data.append('description', description);    
+        data.append('category_id', categories[Number(categoryIndex)].id);
+        data.append('file', image);
+
+        const token = await getCookieClient();
+
+        await api.post("/product", data, {
+            headers: {
+               Authorization: `Bearer ${token}`
+            }
+        })
+        console.log('Product registered');
+    }
+
     function handleFile(event: ChangeEvent<HTMLInputElement>) {
         if(event.target.files && event.target.files[0]){
             const image = event.target.files[0];
@@ -28,12 +63,13 @@ export default function Form({categories}: FormProps) {
             setImage(image);
             setPreviewImage(URL.createObjectURL(image));
         }
+        redirect('/dashboard/product')
     }
   return (
     <main className={styles.container}>
         <h1>New Product</h1>       
 
-        <form className={styles.form}>
+        <form className={styles.form} action={handleRegisterProduct}>
             <label className={styles.labelImagem} >
                 <span>
                     <UploadCloud size={30} color='#fff' />
@@ -62,7 +98,7 @@ export default function Form({categories}: FormProps) {
             </select>
             <input 
                 type="text" 
-                name='name' 
+                name='name-product' 
                 placeholder='Nome do produto' 
                 required  
                 className={styles.input}
